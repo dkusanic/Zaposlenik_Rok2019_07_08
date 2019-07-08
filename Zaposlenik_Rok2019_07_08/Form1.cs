0818e2fc39f5396a436ba33ecd269c72a73fcb35
Domagoj Kusanic <dkusanic@foi.hr>
2019-07-08 13:00:00 +0200

    Add project files.

---
+++ Zaposlenik_Rok2019_07_08/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zaposlenik_Rok2019_07_08
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
